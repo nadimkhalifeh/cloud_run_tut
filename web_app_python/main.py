a18e88bc694b89eb467c99ff1c69110ab7fb608e
NadimKhalifeh <nadim_khalifeh@hotmail.com>
2024-10-01 17:38:52 -0400

    updates

--- web_app_python/main.py
+++ web_app_python/main.py
@@ -11,6 +11,12 @@ def hello_world():
     """Example Hello World route."""
 
     return f"Hello World!!!@@@@!!!"
-    
+
+@app.route("/user", methods=['GET'])
+def get_user():
+    """Example Hello World route."""
+
+    return json.dumps({"id":1 , "Name": "Nadim"})
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
